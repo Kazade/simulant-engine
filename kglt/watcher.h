8813d1949ef1f7e93e44443a191f8e4b3853b12a
Luke Benstead <kazade@gmail.com>
2013-05-06 09:01:31 +0100

    Make the Watcher class work, with tests

--- kglt/watcher.h
+++ kglt/watcher.h
@@ -1,6 +1,10 @@
 #ifndef WATCHER_H
 #define WATCHER_H
 
+#include <unordered_map>
+#include "generic/managed.h"
+#include "kazbase/unicode.h"
+
 namespace kglt {
 
 /**
@@ -14,7 +18,45 @@ namespace kglt {
   watcher->unwatch("path/to/file");
 */
 
+class WindowBase;
 
+enum WatchEvent {
+    WATCH_EVENT_MODIFY,
+    WATCH_EVENT_DELETE,
+    WATCH_EVENT_MOVE
+};
+
+typedef std::function<void (unicode, WatchEvent)> WatchCallback;
+
+class Watcher:
+    public Managed<Watcher> {
+
+public:
+    Watcher();
+    Watcher(WindowBase &window);
+
+    ~Watcher();
+
+    bool init();
+
+    void watch(const unicode& path, WatchCallback cb);
+    void unwatch(const unicode& path);
+
+    bool update();
+
+    void start();
+    void stop();
+private:
+    WindowBase* window_;
+
+    int inotify_fd_;
+    bool watching_;
+    int inotify_flag_;
+
+    std::unordered_map<int, unicode> descriptor_paths_;
+    std::unordered_map<unicode, int> watch_descriptors_;
+    std::unordered_map<int, WatchCallback> watch_callbacks_;
+};
 
 }
 
---
+++ tests/test_watcher.h
@@ -0,0 +1,64 @@
+#ifndef TEST_WATCHER_H
+#define TEST_WATCHER_H
+
+#include "kglt/kazbase/testing.h"
+#include "kglt/kazbase/os.h"
+#include "kglt/watcher.h"
+
+
+class WatcherTest : public TestCase {
+public:
+    void set_up() {
+        watcher = Watcher::create();
+
+        modify_counter = 0;
+        delete_counter = 0;
+        move_counter = 0;
+    }
+
+    void callback(const unicode& filename, WatchEvent event) {
+        if(event == WATCH_EVENT_MODIFY) {
+            modify_counter++;
+        } else if(event == WATCH_EVENT_DELETE) {
+            delete_counter++;
+        } else if(event == WATCH_EVENT_MOVE) {
+            move_counter++;
+        }
+    }
+
+    void test_watching() {
+        unicode test_file = os::path::join(os::temp_dir(), "watcher.test");
+        os::touch(test_file);
+
+        watcher->watch(test_file, std::bind(&WatcherTest::callback, this, std::placeholders::_1, std::placeholders::_2));
+
+        assert_equal(0, modify_counter);
+
+        os::touch(test_file);
+        watcher->update();
+
+        assert_equal(1, modify_counter);
+
+        os::touch(test_file);
+        watcher->update();
+
+        assert_equal(2, modify_counter);
+
+        assert_equal(0, delete_counter);
+
+        os::delete_path(test_file);
+        watcher->update();
+
+        assert_equal(1, delete_counter);
+    }
+
+private:
+    Watcher::ptr watcher;
+
+    int modify_counter;
+    int delete_counter;
+    int move_counter;
+};
+
+
+#endif // TEST_WATCHER_H
