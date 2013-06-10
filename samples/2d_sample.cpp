18cf2f067c58408163e4e08860d4d64629445d98
Luke Benstead <kazade@gmail.com>
2013-06-10 22:22:05 +0100

    Move the 2D sample to the new App class and demonstrate that the Sprite class needs rethinking

--- samples/2d_sample.cpp
+++ samples/2d_sample.cpp
@@ -2,32 +2,43 @@
 #include "kglt/shortcuts.h"
 #include "kglt/additional.h"
 
+class Sample2D: public kglt::App {
+public:
+    Sample2D():
+        App("KGLT Sprite Sample") {
+
+        window().set_logging_level(kglt::LOG_LEVEL_DEBUG);
+    }
+
+private:
+    bool do_init() {
+        //FIXME: This is rubbish, sprites should operator on strips!
+        kglt::extra::SpriteStripLoader loader(stage(), "sample_data/sonic.png", 64);
+
+        //Load the strip of sprites into separate textures
+        std::vector<kglt::TextureID> frames = loader.load_frames();
+
+        //Construct a Sprite object that takes care of handling materials, meshes etc.
+        kglt::extra::Sprite::ptr sprite = kglt::extra::Sprite::create(scene().stage_ref(stage().id()));
+        sprite->add_animation("stand", container::slice(frames, 4, 6), 0.5);
+        sprite->set_render_dimensions(1.5, 1.5);
+        sprite->move_to(0.0, 0.0, -1.0);
+
+        //Automatically calculate an orthographic projection, taking into account the aspect ratio
+        //and the passed height. For example, passing a height of 2.0 would mean the view would extend
+        //+1 and -1 in the vertical direction, -1.0 - +1.0 near/far, and width would be calculated from the aspect
+        //window.scene().pass().viewport().configure(kglt::VIEWPORT_TYPE_BLACKBAR_16_BY_9);
+        scene().camera().set_orthographic_projection_from_height((float) 224 / (float) 40, 16.0 / 9.0);
+
+        return true;
+    }
+
+    void do_step(float dt) {}
+    void do_cleanup() {}
+};
+
+
 int main(int argc, char* argv[]) {
-	//Set up logging to stdio
-	logging::get_logger("/")->add_handler(logging::Handler::ptr(new logging::StdIOHandler));
-
-    kglt::Window::ptr window = kglt::Window::create();
-    kglt::Stage& stage = window->scene().stage();
-
-    window->set_title("KGLT Sprite Sample");
-
-    //Load the strip of sprites into separate textures
-    kglt::extra::SpriteStripLoader loader(stage, "sample_data/sonic.png", 64);
-    std::vector<kglt::TextureID> frames = loader.load_frames();
-
-    //Construct a Sprite object that takes care of handling materials, meshes etc.
-    kglt::extra::Sprite::ptr sprite = kglt::extra::Sprite::create(window->scene().stage_ref(stage.id()));
-    sprite->add_animation("stand", container::slice(frames, 4, 6), 0.5);
-    sprite->set_render_dimensions(1.5, 1.5);
-    sprite->move_to(0.0, 0.0, -1.0);
-
-	//Automatically calculate an orthographic projection, taking into account the aspect ratio
-	//and the passed height. For example, passing a height of 2.0 would mean the view would extend
-	//+1 and -1 in the vertical direction, -1.0 - +1.0 near/far, and width would be calculated from the aspect
-    //window.scene().pass().viewport().configure(kglt::VIEWPORT_TYPE_BLACKBAR_16_BY_9);
-    window->scene().camera().set_orthographic_projection_from_height((float) 224 / (float) 40, 16.0 / 9.0);
-	
-    while(window->update()) {}
-
-	return 0;
+    Sample2D app;
+    return app.run();
 }
