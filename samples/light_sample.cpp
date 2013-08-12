506f429c846559efafc30e25e00f9836dcd25adb
Luke Benstead <kazade@gmail.com>
2013-08-12 09:25:59 +0100

    Restore the light_sample app to its former glory

--- samples/light_sample.cpp
+++ samples/light_sample.cpp
@@ -19,7 +19,7 @@ int main(int argc, char* argv[]) {
     stage.set_ambient_light(kglt::Colour(0.2, 0.2, 0.2, 1.0));
 
     kglt::ActorID actor_id = stage.geom_factory().new_cube(2.0);
-    stage.actor(actor_id)->move_to(0.0, 0.0, -50.0);
+    stage.actor(actor_id)->move_to(0.0, 0.0, -10.0);
 
     kglt::TextureID texture = stage.new_texture_from_file("sample_data/crate.png");
     stage.actor(actor_id)->mesh()->set_texture_on_material(0, texture);
@@ -57,9 +57,9 @@ int main(int argc, char* argv[]) {
     });
 
     while(window->update()) {
-        //stage.actor(actor_id)->rotate_absolute_x(window->delta_time() * 20.0);
-        //stage.actor(actor_id)->rotate_absolute_y(window->delta_time() * 15.0);
-        //stage.actor(actor_id)->rotate_absolute_z(window->delta_time() * 25.0);
+        stage.actor(actor_id)->rotate_absolute_x(window->delta_time() * 20.0);
+        stage.actor(actor_id)->rotate_absolute_y(window->delta_time() * 15.0);
+        stage.actor(actor_id)->rotate_absolute_z(window->delta_time() * 25.0);
     }
 
     return 0;
