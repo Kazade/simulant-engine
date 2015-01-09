6496378e0862df23c27c2cfdc19c89472bd91cdf
Luke Benstead <kazade@gmail.com>
2015-01-09 08:25:49 +0000

    Tone down the logging

--- kglt/window.cpp
+++ kglt/window.cpp
@@ -108,27 +108,27 @@ void Window::check_events() {
                 handle_mouse_motion(event.motion.x, event.motion.y);
             } break;
             case SDL_MOUSEBUTTONDOWN: {
-                L_INFO(_u("MOUSEDOWN received: {0}").format(event.button.button));
+                L_DEBUG(_u("MOUSEDOWN received: {0}").format(event.button.button));
                 handle_mouse_button_down(event.button.button);
             } break;
             case SDL_MOUSEBUTTONUP: {
-                L_INFO(_u("MOUSEUP received: {0}").format(event.button.button));
+                L_DEBUG(_u("MOUSEUP received: {0}").format(event.button.button));
                 handle_mouse_button_up(event.button.button);
             } break;
             case SDL_FINGERDOWN: {
-                L_INFO(_u("FINGERDOWN received: {0} - {1}, {2}").format(event.tfinger.fingerId, event.tfinger.x, event.tfinger.y));
+                L_DEBUG(_u("FINGERDOWN received: {0} - {1}, {2}").format(event.tfinger.fingerId, event.tfinger.x, event.tfinger.y));
                 int x, y;
                 denormalize(event.tfinger.x, event.tfinger.y, x, y);
                 handle_touch_down(event.tfinger.fingerId, x, y);
             } break;
             case SDL_FINGERMOTION: {
-                L_INFO(_u("FINGERMOTION received: {0}, {1}").format(event.tfinger.x, event.tfinger.y));
+                L_DEBUG(_u("FINGERMOTION received: {0}, {1}").format(event.tfinger.x, event.tfinger.y));
                 int x, y;
                 denormalize(event.tfinger.x, event.tfinger.y, x, y);
                 handle_touch_motion(event.tfinger.fingerId, x, y);
             } break;
             case SDL_FINGERUP: {
-                L_INFO(_u("FINGERUP received: {0} - {1}, {2}").format(event.tfinger.fingerId, event.tfinger.x, event.tfinger.y));
+                L_DEBUG(_u("FINGERUP received: {0} - {1}, {2}").format(event.tfinger.fingerId, event.tfinger.x, event.tfinger.y));
                 int x, y;
                 denormalize(event.tfinger.x, event.tfinger.y, x, y);
                 handle_touch_up(event.tfinger.fingerId, x, y);
