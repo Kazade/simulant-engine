9e08a24e2f61e6d442a76b1f22f36113e1bb290f
Luke Benstead <kazade@gmail.com>
2015-01-23 09:00:17 +0000

    Remove default_stage from the LUA api, it doesn't exist anymore

--- kglt/lua/api.cpp
+++ kglt/lua/api.cpp
@@ -66,7 +66,6 @@ void export_lua_api(lua_State* state) {
             .def("delete_stage", &WindowBase::delete_stage)
             .def("show_stats", &WindowBase::show_stats)
             .def("hide_stats", &WindowBase::hide_stats)
-            .property("default_stage", (StagePtr(WindowBase::*)())&WindowBase::stage)
             .property("stage_count", &WindowBase::stage_count)
             .property("width", &WindowBase::width)
             .property("height", &WindowBase::height)
