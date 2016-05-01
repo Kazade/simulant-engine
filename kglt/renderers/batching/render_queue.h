154a20697a1fef1d23f971aa9b0fa57e793246a5
Luke Benstead <kazade@gmail.com>
2016-05-01 13:46:14 +0100

    Continue implementing a much more sensible batching system (WIP)

--- kglt/renderers/batching/render_queue.h
+++ kglt/renderers/batching/render_queue.h
@@ -1,15 +1,27 @@
 #pragma once
 
-#include "../renderer.h"
+#include <list>
+#include "./renderable.h"
 
 namespace kglt {
-namespace new_batcher {
 
 class MaterialPass;
+class Renderer;
+
+namespace new_batcher {
 
 class Batch {
 public:
-    void add_renderable(Renderable* renderable);
+    void add_renderable(Renderable* renderable) {
+        renderables_.push_back(renderable);
+    }
+
+    void each(std::function<void (uint32_t, Renderable*)> func) {
+        uint32_t i = 0;
+        for(auto& renderable: renderables_) {
+            func(i++, renderable);
+        }
+    }
 
 private:
     std::list<Renderable*> renderables_;
@@ -25,8 +37,27 @@ class RenderGroupImpl {
 public:
     typedef std::shared_ptr<RenderGroupImpl> ptr;
 
-    virtual ~RenderGroup() {}
-    virtual bool operator<(const RenderGroup& rhs) const = 0;
+    RenderGroupImpl(RenderPriority priority):
+        priority_(priority) {
+
+    }
+
+    virtual ~RenderGroupImpl() {}
+
+    bool operator<(const RenderGroupImpl& rhs) {
+        // Always sort on priority first
+
+        if(this->priority_ < rhs.priority_) {
+            return true;
+        }
+
+        return lt(rhs);
+    }
+
+private:
+    virtual bool lt(const RenderGroupImpl& rhs) const = 0;
+
+    RenderPriority priority_;
 };
 
 class RenderGroup {
@@ -42,11 +73,22 @@ private:
     RenderGroupImpl::ptr impl_;
 };
 
+
+class RenderGroupFactory {
+public:
+    virtual ~RenderGroupFactory() {}
+
+    virtual RenderGroup new_render_group(Renderable* renderable) = 0;
+};
+
+typedef uint32_t Pass;
+typedef uint32_t Iteration;
+
 class RenderQueue {
 public:
-    typedef std::function<void (Renderable*, MaterialPass*, uint32_t iteration)> TraverseCallback;
+    typedef std::function<void (RenderGroup*, RenderGroup*, Renderable*, MaterialPass*, Iteration)> TraverseCallback;
 
-    RenderQueue(Stage* stage);
+    RenderQueue(Stage* stage, RenderGroupFactory* render_group_factory);
 
     void insert_renderable(Renderable* renderable); // IMPORTANT, must updated RenderGroups if they exist already
     void remove_renderable(Renderable* renderable);
@@ -54,9 +96,16 @@ public:
     void traverse(TraverseCallback callback) const;
 
 private:
+    // std::map is ordered, so by using the RenderGroup as the key we
+    // minimize GL state changes (e.g. if a RenderGroupImpl orders by TextureID, then ShaderID
+    // then we'll see  (TexID(1), ShaderID(1)), (TexID(1), ShaderID(2)) for example meaning the
+    // texture doesn't change even if the shader does
     typedef std::map<RenderGroup, Batch> BatchMap;
+    typedef std::vector<BatchMap> BatchPasses;
 
-    BatchMap batches_;
+    Stage* stage_ = nullptr;
+    RenderGroupFactory* render_group_factory_ = nullptr;
+    BatchPasses batches_;
 };
 
 }
--- kglt/renderers/renderer.h
+++ kglt/renderers/renderer.h
@@ -10,34 +10,11 @@
 #include "../utils/geometry_buffer.h"
 #include "../generic/auto_weakptr.h"
 #include "../window_base.h"
+#include "batching/renderable.h"
 
 namespace kglt {
 
 class SubActor;
-
-class Renderable : public virtual BoundableEntity {
-public:
-    virtual ~Renderable() {}
-
-    virtual const VertexData& vertex_data() const = 0;
-    virtual const IndexData& index_data() const = 0;
-    virtual const MeshArrangement arrangement() const = 0;
-
-    virtual void _update_vertex_array_object() = 0;
-    virtual void _bind_vertex_array_object() = 0;
-
-    virtual RenderPriority render_priority() const = 0;
-    virtual Mat4 final_transformation() const = 0;
-
-    virtual const MaterialID material_id() const = 0;
-    virtual const bool is_visible() const = 0;
-
-    virtual MeshID instanced_mesh_id() const = 0;
-    virtual SubMeshID instanced_submesh_id() const = 0;
-};
-
-typedef std::shared_ptr<Renderable> RenderablePtr;
-
 
 class Renderer {
 public:
