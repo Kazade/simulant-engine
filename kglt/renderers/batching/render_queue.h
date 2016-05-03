af6440e17deb45cde525092ec3a1dcd550039885
Luke Benstead <kazade@gmail.com>
2016-05-03 09:31:43 +0100

    Write a failing test to check texture grouping

--- kglt/renderers/batching/render_queue.h
+++ kglt/renderers/batching/render_queue.h
@@ -95,6 +95,14 @@ public:
 
     void traverse(TraverseCallback callback) const;
 
+    uint32_t pass_count() const { return batches_.size(); }
+    uint32_t group_count(Pass pass_number) const {
+        if(pass_number > batches_.size()) {
+            throw LogicError("Tried to access a pass that doesn't exist");
+        }
+
+        return batches_[pass_number].size();
+    }
 private:
     // std::map is ordered, so by using the RenderGroup as the key we
     // minimize GL state changes (e.g. if a RenderGroupImpl orders by TextureID, then ShaderID
--- tests/test_render_queue.h
+++ tests/test_render_queue.h
@@ -1,4 +1,77 @@
-#ifndef TEST_RENDER_QUEUE_H
-#define TEST_RENDER_QUEUE_H
+#pragma once
 
-#endif // TEST_RENDER_QUEUE_H
+#include <kaztest/kaztest.h>
+
+#include "kglt/kglt.h"
+
+namespace {
+
+using namespace kglt;
+
+class RenderQueueTests : public KGLTTestCase {
+public:
+    void set_up() {
+        KGLTTestCase::set_up();
+        stage_ = window->stage(window->new_stage());
+    }
+
+    void tear_down() {
+        window->delete_stage(stage_->id());
+    }
+
+    void test_texture_grouping() {
+        auto texture_1 = stage_->new_texture();
+        auto texture_2 = stage_->new_texture();
+
+        auto mat_1 = stage_->new_material_from_texture(texture_1);
+        auto mat_2 = stage_->new_material_from_texture(texture_2);
+        auto mat_3 = stage_->new_material_from_texture(texture_1); // Texture 1 repeated
+
+        auto& render_queue = stage_->render_queue;
+
+        assert_equal(0, render_queue->pass_count());
+
+        auto mesh_1 = stage_->new_mesh_as_cube(1.0);
+        stage_->mesh(mesh_1)->set_material_id(mat_1);
+
+        auto mesh_2 = stage_->new_mesh_as_cube(1.0);
+        stage_->mesh(mesh_2)->set_material_id(mat_2);
+
+        auto mesh_3 = stage_->new_mesh_as_cube(1.0);
+        stage_->mesh(mesh_3)->set_material_id(mat_3);
+
+        stage_->new_actor_with_mesh(mesh_1);
+
+        assert_equal(1, render_queue->pass_count());
+        assert_equal(1, render_queue->group_count(0));
+
+        stage_->new_actor_with_mesh(mesh_1);
+
+        assert_equal(1, render_queue->pass_count());
+        assert_equal(2, render_queue->group_count(0));
+
+        stage_->new_actor_with_mesh(mesh_2);
+
+        assert_equal(2, render_queue->pass_count());
+        assert_equal(2, render_queue->group_count(0));
+        assert_equal(1, render_queue->group_count(1));
+
+        stage_->new_actor_with_mesh(mesh_3);
+
+        assert_equal(2, render_queue->pass_count());
+        assert_equal(3, render_queue->group_count(0));
+        assert_equal(1, render_queue->group_count(1));
+    }
+
+#ifdef KGLT_GL_VERSION_2X
+    void test_shader_grouping() {
+
+    }
+#endif
+
+private:
+    StagePtr stage_;
+
+};
+
+}
