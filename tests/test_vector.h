fbfea553c70e94821654282e631c0b8bbfc2fa05
Luke Benstead <kazade@gmail.com>
2016-05-31 09:05:37 +0100

    Set Debug as the default build type.
    Before we were explicitly passing -g which meant it wasn't possible to do a release build.
    
    This commit also removes some leftover AI stuff.

--- tests/test_vector.h
+++ tests/test_vector.h
@@ -4,7 +4,6 @@
 #include "kaztest/kaztest.h"
 
 #include "kglt/kglt.h"
-#include "kglt/extra/ai/pid.h"
 
 class VectorTest : public TestCase {
 public:
@@ -23,20 +22,6 @@ public:
         up *= 10;
 
         assert_equal(kglt::Vec2(0, 10), up);
-    }
-
-    void test_vec3_pid_controller() {
-        auto controller = kglt::extra::Vec3PIDController::create(20, 0, 0.25);
-
-        kglt::Vec3 current(1, 0, 0);
-        kglt::Vec3 desired(-1, 0, 0);
-
-        for(int i = 0; i < 30; ++i) {
-            current += controller->update(desired - current, 1.0 / 60).limit(0.1);
-        }
-
-        assert_close(desired.x, current.x, 0.001);
-
     }
 };
 
