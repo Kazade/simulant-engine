20dff90dc839e873823fc8a468541004ae2c00bb
Luke Benstead <kazade@gmail.com>
2016-07-23 10:29:46 +0100

    Start implementing GUI management tools for creating projects and building them for different platforms

---
+++ tools/dreamfire/dreamfire-admin/DreamfireAdmin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace dreamfireadmin
+{
+	public class DreamfireAdmin : Form
+	{
+		Label title = new Label();
+
+		public DreamfireAdmin ()
+		{
+			this.SuspendLayout ();
+			this.title.AutoSize = true;
+			this.title.Text = "Start a New Dreamfire Project";
+
+			this.Controls.Add (title);
+			this.ResumeLayout ();
+
+			this.Name = "Dreamfire Admin";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Dreamfire Admin";
+		}
+	}
+
+	public class Program {
+		public static void Main(String [] args) {
+			Application.EnableVisualStyles ();
+			Application.SetCompatibleTextRenderingDefault (false);
+			Application.Run (new DreamfireAdmin ());
+		}
+	}
+}
+
