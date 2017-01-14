a0e6863a688c4c6a29ee064ece9df8ad74fe12f3
Luke Benstead <kazade@gmail.com>
2017-01-14 14:34:09 +0000

    Add a UI demo for testing

---
+++ samples/ui_demo.cpp
@@ -0,0 +1,43 @@
+
+#include <simulant/simulant.h>
+
+class MainScreen:
+    public smlt::Screen<MainScreen> {
+
+public:
+    MainScreen(smlt::WindowBase& window):
+        smlt::Screen<MainScreen>(window, "UI Demo") {}
+
+    void do_load() {
+        camera_ = window->new_camera_with_orthographic_projection(0, window->width(), 0, window->height());
+        stage_ = window->new_stage();
+        window->render(stage_, camera_).with_clear();
+
+        smlt::StagePtr stage = stage_.fetch();
+
+        //stage->ui->transform_input_with_camera(camera_);
+
+        auto title = stage->ui->new_widget_as_label("UI Sample demonstrating widgets");
+        title.fetch()->move_to(window->coordinate_from_normalized(0.5, 0.9));
+
+        auto button = stage->ui->new_widget_as_button("Button 1");
+        button.fetch()->move_to(window->coordinate_from_normalized(0.1, 0.25));
+    }
+
+private:
+    smlt::StageID stage_;
+    smlt::CameraID camera_;
+};
+
+class App : public smlt::Application {
+public:
+    bool do_init() {
+        register_screen("/", smlt::screen_factory<MainScreen>());
+        return true;
+    }
+};
+
+int main(int argc, char* argv[]) {
+    App app;
+    return app.run();
+}
