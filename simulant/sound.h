077b0d024351158a7e1744561aecd9e521b27b07
Luke Benstead <kazade@gmail.com>
2017-04-07 10:07:41 +0100

    Fix up OpenAL includes on OSX

--- simulant/sound.h
+++ simulant/sound.h
@@ -22,8 +22,13 @@
 #include <vector>
 #include <list>
 
+#ifdef __APPLE__
+#include <OpenAL/al.h>
+#include <OpenAL/alc.h>
+#else
 #include <AL/al.h>
 #include <AL/alc.h>
+#endif
 
 #include "generic/managed.h"
 #include "generic/identifiable.h"
