fd6e3f67b1ca3a27b08ed2a5d94f58c4986bc227
Luke Benstead <kazade@gmail.com>
2017-05-08 21:15:47 +0100

    Add dc-build.sh

---
+++ dc-build.sh
@@ -0,0 +1,1 @@
+sudo docker run -v `pwd`:/simulant:Z kazade/dreamcast-sdk /bin/sh -c "source /etc/bash.bashrc; mkdir -p /simulant/dbuild; cd /simulant/dbuild; cmake -DCMAKE_TOOLCHAIN_FILE=/simulant/toolchains/Dreamcast.cmake .. &&  make"
