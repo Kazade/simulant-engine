876f83b687952ec8e16eca0f6b09529fbb1c3188
Luke Benstead <kazade@gmail.com>
2018-01-31 18:33:52 +0000

    Start stubbing out a faster render queue structure

--- simulant/renderers/batching/render_list.h
+++ simulant/renderers/batching/render_list.h
@@ -1,4 +1,62 @@
-#ifndef RENDER_LIST_H
-#define RENDER_LIST_H
+#pragma once
 
-#endif // RENDER_LIST_H
+#include <set>
+#include <vector>
+#include <memory>
+
+#include "../../types.h"
+#include "render_queue.h"
+
+
+#ifdef _arch_dreamcast
+// Dreamcast has more limited ram
+#define MAX_NODES_PER_BATCH 4096
+#else
+#define MAX_NODES_PER_BATCH 16384
+#endif
+
+namespace smlt {
+
+class HardwareBuffer;
+
+/* A single renderable thing */
+struct RenderNode {
+    MaterialID material_id;
+    MeshArrangement arrangement;
+    IndexType index_type;
+    std::size_t index_element_count;
+
+    HardwareBuffer* index_buffer = nullptr;
+    HardwareBuffer* vertex_buffer = nullptr;
+
+    Mat4 transform;
+
+    /*
+     * Set to false if the renderable is invisible
+     * or not returned by the partitioner
+     */
+    bool is_visible = true;
+};
+
+struct RenderNodeReference {
+
+}
+
+/* Renderable things grouped by material properties */
+struct RenderBatch {
+    std::unique_ptr<RenderGroup> group;
+
+    /* Each node is a single draw call */
+    std::vector<RenderNode> nodes;
+
+    /* Materials duplicated for cache locality */
+    std::unordered_map<MaterialID, Material> materials;
+};
+
+typedef std::set<RenderBatch> RenderList;
+
+void traverse_list(const RenderList& list, batcher::RenderQueueVisitor* visitor) {
+    visitor->start_traversal()
+}
+
+}
