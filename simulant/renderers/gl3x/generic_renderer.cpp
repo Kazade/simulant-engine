3ef97875a9454d930d52beadb91bff8a058c0bca
Luke Benstead <kazade@gmail.com>
2018-06-30 14:01:02 +0100

    glShadeModel *isn't* deprecated in GL 3.0 so let's use it

--- simulant/renderers/gl3x/generic_renderer.cpp
+++ simulant/renderers/gl3x/generic_renderer.cpp
@@ -437,6 +437,14 @@ void GL4RenderQueueVisitor::change_material_pass(const MaterialPass* prev, const
         renderer_->set_blending_mode(next->blending());
     }
 
+    if(!prev || prev->shade_model() != next->shade_model()) {
+        if(next->shade_model() == SHADE_MODEL_SMOOTH) {
+            GLCheck(glShadeModel, GL_SMOOTH);
+        } else {
+            GLCheck(glShadeModel, GL_FLAT);
+        }
+    }
+
     renderer_->set_stage_uniforms(next, program_, global_ambient_);
     renderer_->set_material_uniforms(next, program_);
 
