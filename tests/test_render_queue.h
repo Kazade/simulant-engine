5944cc85442405117be1477a68d74b6d79e60b3e
Luke Benstead <kazade@gmail.com>
2018-08-27 20:04:24 +0100

    Fix some failing tests

--- tests/test_render_queue.h
+++ tests/test_render_queue.h
@@ -26,14 +26,8 @@ public:
         auto texture_2 = stage_->assets->new_texture();
 
         auto mat_1 = stage_->assets->new_material_from_texture(texture_1);
-#ifndef SIMULANT_GL_VERSION_1X
-        mat_1.fetch()->delete_pass(1); // Delete the lighting pass from the material
-#endif
-
         auto mat_2 = stage_->assets->new_material_from_texture(texture_2);
-#ifndef SIMULANT_GL_VERSION_1X
-        mat_2.fetch()->delete_pass(1);
-#endif
+
         auto mesh_1 = stage_->assets->new_mesh_as_cube(1.0);
         stage_->assets->mesh(mesh_1)->set_material_id(mat_1);
 
@@ -88,14 +82,15 @@ public:
         auto mesh_1 = stage_->assets->new_mesh_as_cube(1.0);
         auto actor = stage_->new_actor_with_mesh(mesh_1);
 
-#ifdef SIMULANT_GL_VERSION_1X
-        assert_equal(1u, render_queue->pass_count());
-        assert_equal(1u, render_queue->group_count(0));
-#else
-        assert_equal(2u, render_queue->pass_count());
-        assert_equal(1u, render_queue->group_count(0));
-        assert_equal(1u, render_queue->group_count(1));
-#endif
+        if(window->renderer->name() == "gl1x") {
+            assert_equal(1u, render_queue->pass_count());
+            assert_equal(1u, render_queue->group_count(0));
+        } else {
+            assert_equal(2u, render_queue->pass_count());
+            assert_equal(1u, render_queue->group_count(0));
+            assert_equal(1u, render_queue->group_count(1));
+        }
+
         stage_->delete_actor(actor->id());
 
         assert_equal(0u, render_queue->pass_count());
@@ -106,17 +101,9 @@ public:
         auto texture_2 = stage_->assets->new_texture();
 
         auto mat_1 = stage_->assets->new_material_from_texture(texture_1);
-#ifndef SIMULANT_GL_VERSION_1X
-        mat_1.fetch()->delete_pass(1);
-#endif
         auto mat_2 = stage_->assets->new_material_from_texture(texture_2);
-#ifndef SIMULANT_GL_VERSION_1X
-        mat_2.fetch()->delete_pass(1);
-#endif
         auto mat_3 = stage_->assets->new_material_from_texture(texture_1); // Texture 1 repeated
-#ifndef SIMULANT_GL_VERSION_1X
-        mat_3.fetch()->delete_pass(1);
-#endif
+
         auto& render_queue = stage_->render_queue;
 
         assert_equal(0u, render_queue->pass_count());
