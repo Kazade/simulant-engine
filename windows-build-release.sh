9b6918921845c584c2ea449b5e30b5646223d5d5
Hayden Kowalchuk <mrneo240@users.noreply.github.com>
2018-10-17 10:47:48 -0400

    More Cmake Cleanup

--- windows-build-release.sh
+++ windows-build-release.sh
@@ -1,1 +1,1 @@
-sh -c "mkdir -p buildw; cd buildw; mingw64 cmake -DCMAKE_TOOLCHAIN_FILE=/home/dev/dc/simulant-engine/toolchains/Mingw.cmake -DCMAKE_BUILD_TYPE=Release -DHOST=x86_64-w64-mingw32 .. && mingw64 make -j5"
+sh -c "mkdir -p buildw; cd buildw; cmake -DCMAKE_TOOLCHAIN_FILE=/home/dev/dc/simulant-engine/toolchains/Mingw.cmake -DCMAKE_BUILD_TYPE=Release  .. &&  make -j5"
