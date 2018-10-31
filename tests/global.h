a7a1679a224920cdbceabcf13e7ad1651e0dd660
Luke Benstead <kazade@gmail.com>
2018-10-31 20:18:14 +0000

    Enable logging earlier in the tests

--- tests/global.h
+++ tests/global.h
@@ -15,27 +15,7 @@ extern smlt::SDL2Window::ptr window;
 
 class SimulantTestCase : public TestCase {
 public:
-    void set_up() {
-        if(!window) {
-#ifdef _arch_dreamcast
-            window = smlt::KOSWindow::create(nullptr, 640, 480, 32, false, true);
-#else
-            window = smlt::SDL2Window::create(nullptr, 640, 480, 0, false, true);
-#endif
-            window->_init();
-
-            if(std::getenv("SIMULANT_DEBUG")) {
-                kazlog::get_logger("/")->add_handler(kazlog::Handler::ptr(new kazlog::StdIOHandler));
-                window->set_logging_level(smlt::LOG_LEVEL_DEBUG);
-            } else {
-                window->set_logging_level(smlt::LOG_LEVEL_NONE);
-            }
-
-            window->resource_locator->add_search_path("sample_data");
-        } else {
-            window->reset();
-        }
-    }
+    void set_up();
 };
 
 
