8e2c592c61ae0a17686f1441a9045c76c32c602c
Luke Benstead <kazade@gmail.com>
2018-11-16 16:31:30 +0000

    Fix Dreamcast toolchain file

--- toolchains/Dreamcast.cmake
+++ toolchains/Dreamcast.cmake
@@ -4,12 +4,12 @@ SET(CMAKE_SYSTEM_VERSION 1)
 
 set(CMAKE_CROSSCOMPILING TRUE)
 
-set(CMAKE_C_COMPILER "kos-cc")
-set(CMAKE_CXX_COMPILER "kos-c++")
-set(CMAKE_AR "kos-ar" CACHE FILEPATH "Archiver")
-set(CMAKE_RANLIB "kos-ranlib" CACHE FILEPATH "Ranlib")
-set(CMAKE_ASM_COMPILER "kos-as")
-set(CMAKE_LINKER "kos-c++")
+set(CMAKE_C_COMPILER "$ENV{KOS_CC_BASE}/bin/$ENV{KOS_CC_PREFIX}-gcc")
+set(CMAKE_CXX_COMPILER "$ENV{KOS_CC_BASE}/bin/$ENV{KOS_CC_PREFIX}-g++")
+set(CMAKE_AR "$ENV{KOS_CC_BASE}/bin/$ENV{KOS_CC_PREFIX}-ar" CACHE FILEPATH "Archiver")
+set(CMAKE_RANLIB "$ENV{KOS_CC_BASE}/bin/$ENV{KOS_CC_PREFIX}-ranlib" CACHE FILEPATH "Ranlib")
+set(CMAKE_ASM_COMPILER "$ENV{KOS_CC_BASE}/bin/$ENV{KOS_CC_PREFIX}-as")
+set(CMAKE_LINKER "$ENV{KOS_CC_BASE}/bin/$ENV{KOS_CC_PREFIX}-g++")
 set(CMAKE_FIND_LIBRARY_SUFFIXES ".a")
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 
@@ -18,22 +18,42 @@ set(CMAKE_SYSTEM_INCLUDE_PATH "$ENV{KOS_BASE}/include")
 SET(CMAKE_EXECUTABLE_SUFFIX ".elf")
 SET(CMAKE_EXECUTABLE_SUFFIX_CXX ".elf")
 
-# Make sure we clear any flags set by the environment variables
-SET(ENV{KOS_CFLAGS} $ENV{KOS_INC_PATHS} "-fno-builtin -fno-strict-aliasing")
-SET(ENV{KOS_CPPFLAGS} $ENV{KOS_INC_PATHS})
+
+INCLUDE_DIRECTORIES(
+    $ENV{KOS_BASE}/include
+    $ENV{KOS_BASE}/kernel/arch/$ENV{KOS_ARCH}/include
+    $ENV{KOS_BASE}/addons/include
+    $ENV{KOS_PORTS}/include
+)
+
+LINK_DIRECTORIES(
+    $ENV{KOS_BASE}/lib/$ENV{KOS_ARCH}
+    $ENV{KOS_BASE}/addons/lib/$ENV{KOS_ARCH}
+    $ENV{KOS_PORTS}/lib
+)
+
+LINK_LIBRARIES(-Wl,--start-group -lkallisti -lc -lgcc -Wl,--end-group)
+
+ADD_DEFINITIONS(
+    -D__DREAMCAST__
+    -DDREAMCAST
+    -D_arch_dreamcast
+    -D__arch_dreamcast
+    -D_arch_sub_pristine
+)
 
 SET(
     CMAKE_C_FLAGS
     ${CMAKE_C_FLAGS}
-    "-D__DREAMCAST__ -DDREAMCAST -D__arch_dreamcast -D_arch_sub_pristine"
+    "-fno-builtin -fno-strict-aliasing"
 )
 
-# Optimise for size by default
 SET(
     CMAKE_CXX_FLAGS
     ${CMAKE_CXX_FLAGS}
-    "-Os -D__DREAMCAST__ -DDREAMCAST -D__arch_dreamcast -D_arch_sub_pristine"
+    "-fno-builtin -fno-strict-aliasing"
 )
+
 
 SET(CMAKE_ASM_FLAGS "")
 SET(CMAKE_ASM_FLAGS_RELEASE "")
