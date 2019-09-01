b7e4d9a1dacd938b73abe40bdf2990da40633d81
Luke Benstead <kazade@gmail.com>
2019-09-01 20:25:45 +0100

    Implement a loose Quadtree based on the Octree

--- simulant/nodes/geoms/loose_quadtree.h
+++ simulant/nodes/geoms/loose_quadtree.h
@@ -7,14 +7,12 @@
 
 namespace smlt {
 
-namespace _loose_octree {
-    static constexpr int64_t ipow(int64_t base, int exp, int64_t result = 1) {
-      return exp < 1 ? result : ipow(base*base, exp/2, (exp % 2) ? result*base : result);
-    }
+static constexpr int64_t ipow(int64_t base, int exp, int64_t result = 1) {
+  return exp < 1 ? result : ipow(base*base, exp/2, (exp % 2) ? result*base : result);
 }
 
 /*
- * Fast loose-octree implementation with O(1) insertions, built on a linear
+ * Fast loose-Quadtree implementation with O(1) insertions, built on a linear
  * array for good cache locality.
  *
  * TreeData is a structure which contains data needed across the whole tree,
@@ -28,20 +26,20 @@ typedef uint8_t Level;
 typedef uint16_t GridCoord;
 
 template<typename TreeData, typename NodeData>
-class Octree {
+class Quadtree {
 public:
     struct Node {
         Level level = 0;
-        GridCoord grid[3] = {0, 0, 0};
+        GridCoord grid[2] = {0, 0};
         NodeData* data = nullptr;
     };
 
-    using TraverseCallback = void(Octree::Node*);
+    using TraverseCallback = void(Quadtree::Node*);
 
     typedef TreeData tree_data_type;
     typedef NodeData node_data_type;
 
-    Octree(const AABB& bounds, uint8_t max_level_count=4, TreeData* tree_data=nullptr):
+    Quadtree(const AABB& bounds, uint8_t max_level_count=4, TreeData* tree_data=nullptr):
         tree_data_(tree_data),
         root_width_(bounds.max_dimension()),
         bounds_(bounds),
@@ -51,7 +49,7 @@ public:
         float maxd = root_width_;
         auto halfd = maxd / 2.0;
 
-        auto half = Vec3(halfd, halfd, halfd);
+        auto half = Vec3(halfd, 0, halfd);
         bounds_.set_min(centre_ - half);
         bounds_.set_max(centre_ + half);
 
@@ -59,7 +57,7 @@ public:
         grow(max_level_count);
     }
 
-    virtual ~Octree() {
+    virtual ~Quadtree() {
         /* Make sure we delete the node data */
         for(auto& node: nodes_) {
             delete node.data;
@@ -71,7 +69,7 @@ public:
         return node.level == (levels_ - 1);
     }
 
-    Octree::Node* find_destination_for_sphere(const Vec3& centre, float radius) {
+    Quadtree::Node* find_destination_for_sphere(const Vec3& centre, float radius) {
         auto diameter = radius * 2;
         auto level_and_node_width = level_for_width(diameter);
 
@@ -79,36 +77,31 @@ public:
         auto half_width = root_width_ * 0.5;
 
         assert(centre.x <= bounds_.max().x && centre.x >= bounds_.min().x);
-        assert(centre.y <= bounds_.max().y && centre.y >= bounds_.min().y);
         assert(centre.z <= bounds_.max().z && centre.z >= bounds_.min().z);
 
         auto x = (GridCoord) ((centre.x + half_width - centre_.x) / level_and_node_width.second);
-        auto y = (GridCoord) ((centre.y + half_width - centre_.y) / level_and_node_width.second);
         auto z = (GridCoord) ((centre.z + half_width - centre_.z) / level_and_node_width.second);
 
         assert(x >= 0);
-        assert(y >= 0);
         assert(z >= 0);
 
-        auto level_width = _loose_octree::ipow(2, level_and_node_width.first);
+        auto level_width = ipow(2, level_and_node_width.first);
         assert(x <= level_width);
-        assert(y <= level_width);
         assert(z <= level_width);
 
         /* This handles the case that the center was on the cusp of the cell
          * in which case the above calculation will result in an out of range
          * index */
         x = std::min<GridCoord>(x, level_width - 1);
-        y = std::min<GridCoord>(y, level_width - 1);
         z = std::min<GridCoord>(z, level_width - 1);
 
-        auto idx = calc_index(level_and_node_width.first, x, y, z);
+        auto idx = calc_index(level_and_node_width.first, x, z);
         assert(idx < nodes_.size());
 
         return &nodes_[idx];
     }
 
-    Octree::Node* find_destination_for_triangle(const Vec3* vertices) {
+    Quadtree::Node* find_destination_for_triangle(const Vec3* vertices) {
         /*
          * Return the node that this triangle should be
          * inserted into (the actual insertion won't happen as that's implementation specific
@@ -121,12 +114,12 @@ public:
         return find_destination_for_sphere(centre, radius);
     }
 
-    void traverse(std::function<void (Octree::Node*)> cb) {
+    void traverse(std::function<void (Quadtree::Node*)> cb) {
         if(nodes_.empty()) {
             return;
         }
 
-        std::function<void (Octree::Node&)> visitor = [&](Octree::Node& node) {
+        std::function<void (Quadtree::Node&)> visitor = [&](Quadtree::Node& node) {
             cb(&node);
 
             if(!is_leaf(node)) {
@@ -157,7 +150,7 @@ public:
 
 private:
     template<typename Callback>
-    void _visible_visitor(const Frustum& frustum, const Callback& callback, Octree::Node& node) {
+    void _visible_visitor(const Frustum& frustum, const Callback& callback, Quadtree::Node& node) {
         auto bounds = calc_loose_bounds(node);
         if(frustum.intersects_cube(bounds.centre(), bounds.max_dimension())) {
             callback(&node);
@@ -206,7 +199,7 @@ private:
         return std::make_pair(depth - 1, node_width * 2);
     }
 
-    AABB calc_loose_bounds(const Octree::Node& node) const {
+    AABB calc_loose_bounds(const Quadtree::Node& node) const {
         /* Returns the loose bounds of the cell which is
          * twice the size, but with the same centre */
 
@@ -215,55 +208,51 @@ private:
 
         float r = bounds.width();
 
-        Vec3 diff(r, r, r);
+        Vec3 diff(r, 0, r);
         bounds.set_min(centre - diff);
         bounds.set_max(centre + diff);
         return bounds;
     }
 
-    AABB calc_bounds(const Octree::Node& node) const {
-        auto grid_width = _loose_octree::ipow(2, node.level);
+    AABB calc_bounds(const Quadtree::Node& node) const {
+        auto grid_width = ipow(2, node.level);
         auto cell_width = bounds_.max_dimension() / grid_width;
 
-        Vec3 min(node.grid[0] * cell_width, node.grid[1] * cell_width, node.grid[2] * cell_width);
+        Vec3 min(node.grid[0] * cell_width, 0, node.grid[1] * cell_width);
         min += bounds_.min();
 
-        Vec3 max = min + Vec3(cell_width, cell_width, cell_width);
+        Vec3 max = min + Vec3(cell_width, 0, cell_width);
         return AABB(min, max);
     }
 
     static uint32_t calc_base(uint8_t level) {
         if(level == 0) return 0;
         if(level == 1) return 1;
-        return calc_base(level - 1) + _loose_octree::ipow(_loose_octree::ipow(2, level - 1), 3);
+        return calc_base(level - 1) + ipow(ipow(2, level - 1), 2);
     }
 
-    static uint32_t calc_index(Level k, GridCoord x, GridCoord y, GridCoord z) {
+    static uint32_t calc_index(Level k, GridCoord x, GridCoord z) {
         auto level_base = calc_base(k);
-        auto level_width = _loose_octree::ipow(2, k);
+        auto level_width = ipow(2, k);
 
         assert(x < level_width);
-        assert(y < level_width);
         assert(z < level_width);
 
-        auto idx = x + level_width * y + level_width * level_width * z;
+        auto idx = x + level_width * z;
         return level_base + idx;
     }
 
-    static std::vector<uint32_t> child_indexes(Octree::Node& node) {
+    static std::vector<uint32_t> child_indexes(Quadtree::Node& node) {
         std::vector<uint32_t> indexes;
-        indexes.reserve(8);
+        indexes.reserve(4);
 
         for(uint32_t z = 0; z <= 1; ++z) {
-            for(uint32_t y = 0; y <= 1; ++y) {
-                for(uint32_t x = 0; x <= 1; ++x) {
-                    indexes.push_back(calc_index(
-                        node.level + 1,
-                        2 * node.grid[0] + x,
-                        2 * node.grid[1] + y,
-                        2 * node.grid[2] + z
-                    ));
-                }
+            for(uint32_t x = 0; x <= 1; ++x) {
+                indexes.push_back(calc_index(
+                    node.level + 1,
+                    2 * node.grid[0] + x,
+                    2 * node.grid[1] + z
+                ));
             }
         }
 
@@ -274,33 +263,30 @@ private:
 
         auto required_nodes = 0;
         for(uint8_t i = 0; i < required_levels; ++i) {
-            auto nodes_across = _loose_octree::ipow(2, i);
-            required_nodes += _loose_octree::ipow(nodes_across, 3);
+            auto nodes_across = ipow(2, i);
+            required_nodes += ipow(nodes_across, 2);
         }
 
         levels_ = required_levels;
-        nodes_.resize(required_nodes, Octree::Node());
+        nodes_.resize(required_nodes, Quadtree::Node());
 
         for(auto k = 0; k < levels_; ++k) {
-            auto nodes_across = _loose_octree::ipow(2, k);
+            auto nodes_across = ipow(2, k);
 
             for(GridCoord z = 0; z < nodes_across; ++z) {
-                for(GridCoord y = 0; y < nodes_across; ++y) {
-                    for(GridCoord x = 0; x < nodes_across; ++x) {
-                        auto idx = calc_index(k, x, y, z);
-                        assert(idx < nodes_.size());
-                        assert(idx >= 0);
+                for(GridCoord x = 0; x < nodes_across; ++x) {
+                    auto idx = calc_index(k, x, z);
+                    assert(idx < nodes_.size());
+                    assert(idx >= 0);
 
-                        auto& new_node = nodes_[idx];
+                    auto& new_node = nodes_[idx];
 
-                        assert(!new_node.data);
+                    assert(!new_node.data);
 
-                        new_node.grid[0] = x;
-                        new_node.grid[1] = y;
-                        new_node.grid[2] = z;
-                        new_node.level = k;
-                        new_node.data = new NodeData();
-                    }
+                    new_node.grid[0] = x;
+                    new_node.grid[1] = z;
+                    new_node.level = k;
+                    new_node.data = new NodeData();
                 }
             }
         }
@@ -313,9 +299,9 @@ private:
     Vec3 centre_;
 
     Level levels_ = 0;
-    std::vector<Octree::Node> nodes_;
+    std::vector<Quadtree::Node> nodes_;
 
-    friend class LooseOctreeTests;
+    friend class LooseQuadtreeTests;
 };
 
 }
