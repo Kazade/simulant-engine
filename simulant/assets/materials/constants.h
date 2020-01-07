3f480824561f922f31f522403eeaadea417df43c
Luke Benstead <kazade@gmail.com>
2020-01-07 10:00:30 +0000

    Fix ridiculous code-bloating issue

--- simulant/assets/materials/constants.h
+++ simulant/assets/materials/constants.h
@@ -18,42 +18,42 @@ enum BlendType {
     BLEND_ONE_ONE_MINUS_ALPHA
 };
 
-const std::string DIFFUSE_PROPERTY = "s_material_diffuse";
-const std::string AMBIENT_PROPERTY = "s_material_ambient";
-const std::string SPECULAR_PROPERTY = "s_material_specular";
-const std::string SHININESS_PROPERTY = "s_material_shininess";
-const std::string DIFFUSE_MAP_PROPERTY = "s_diffuse_map";
-const std::string LIGHT_MAP_PROPERTY = "s_light_map";
-const std::string NORMAL_MAP_PROPERTY = "s_normal_map";
-const std::string SPECULAR_MAP_PROPERTY = "s_specular_map";
-const std::string DEPTH_WRITE_ENABLED_PROPERTY = "s_depth_write_enabled";
-const std::string DEPTH_TEST_ENABLED_PROPERTY = "s_depth_test_enabled";
-const std::string DEPTH_FUNC_PROPERTY = "s_depth_func";
-const std::string BLEND_FUNC_PROPERTY = "s_blend_func";
-const std::string CULL_MODE_PROPERTY = "s_cull_mode";
-const std::string SHADE_MODEL_PROPERTY = "s_shade_model";
-const std::string LIGHTING_ENABLED_PROPERTY = "s_lighting_enabled";
-const std::string TEXTURING_ENABLED_PROPERTY = "s_texturing_enabled";
-const std::string POINT_SIZE_PROPERTY = "s_point_size";
-const std::string POLYGON_MODE_PROPERTY = "s_polygon_mode";
-const std::string COLOUR_MATERIAL_PROPERTY = "s_colour_material";
-const std::string LIGHT_POSITION_PROPERTY = "s_light_position";
-const std::string LIGHT_AMBIENT_PROPERTY = "s_light_ambient";
-const std::string LIGHT_DIFFUSE_PROPERTY = "s_light_diffuse";
-const std::string LIGHT_SPECULAR_PROPERTY = "s_light_specular";
-const std::string LIGHT_CONSTANT_ATTENUATION_PROPERTY = "s_light_constant_attenuation";
-const std::string LIGHT_LINEAR_ATTENUATION_PROPERTY = "s_light_linear_attenuation";
-const std::string LIGHT_QUADRATIC_ATTENUATION_PROPERTY = "s_light_quadratic_attenuation";
+const char* const DIFFUSE_PROPERTY = "s_material_diffuse";
+const char* const AMBIENT_PROPERTY = "s_material_ambient";
+const char* const SPECULAR_PROPERTY = "s_material_specular";
+const char* const SHININESS_PROPERTY = "s_material_shininess";
+const char* const DIFFUSE_MAP_PROPERTY = "s_diffuse_map";
+const char* const LIGHT_MAP_PROPERTY = "s_light_map";
+const char* const NORMAL_MAP_PROPERTY = "s_normal_map";
+const char* const SPECULAR_MAP_PROPERTY = "s_specular_map";
+const char* const DEPTH_WRITE_ENABLED_PROPERTY = "s_depth_write_enabled";
+const char* const DEPTH_TEST_ENABLED_PROPERTY = "s_depth_test_enabled";
+const char* const DEPTH_FUNC_PROPERTY = "s_depth_func";
+const char* const BLEND_FUNC_PROPERTY = "s_blend_func";
+const char* const CULL_MODE_PROPERTY = "s_cull_mode";
+const char* const SHADE_MODEL_PROPERTY = "s_shade_model";
+const char* const LIGHTING_ENABLED_PROPERTY = "s_lighting_enabled";
+const char* const TEXTURING_ENABLED_PROPERTY = "s_texturing_enabled";
+const char* const POINT_SIZE_PROPERTY = "s_point_size";
+const char* const POLYGON_MODE_PROPERTY = "s_polygon_mode";
+const char* const COLOUR_MATERIAL_PROPERTY = "s_colour_material";
+const char* const LIGHT_POSITION_PROPERTY = "s_light_position";
+const char* const LIGHT_AMBIENT_PROPERTY = "s_light_ambient";
+const char* const LIGHT_DIFFUSE_PROPERTY = "s_light_diffuse";
+const char* const LIGHT_SPECULAR_PROPERTY = "s_light_specular";
+const char* const LIGHT_CONSTANT_ATTENUATION_PROPERTY = "s_light_constant_attenuation";
+const char* const LIGHT_LINEAR_ATTENUATION_PROPERTY = "s_light_linear_attenuation";
+const char* const LIGHT_QUADRATIC_ATTENUATION_PROPERTY = "s_light_quadratic_attenuation";
 
-const std::string VIEW_MATRIX_PROPERTY = "s_view";
-const std::string MODELVIEW_PROJECTION_MATRIX_PROPERTY = "s_modelview_projection";
-const std::string PROJECTION_MATRIX_PROPERTY = "s_projection";
-const std::string MODELVIEW_MATRIX_PROPERTY = "s_modelview";
-const std::string INVERSE_TRANSPOSE_MODELVIEW_MATRIX_PROPERTY = "s_inverse_transpose_modelview";
+const char* const VIEW_MATRIX_PROPERTY = "s_view";
+const char* const MODELVIEW_PROJECTION_MATRIX_PROPERTY = "s_modelview_projection";
+const char* const PROJECTION_MATRIX_PROPERTY = "s_projection";
+const char* const MODELVIEW_MATRIX_PROPERTY = "s_modelview";
+const char* const INVERSE_TRANSPOSE_MODELVIEW_MATRIX_PROPERTY = "s_inverse_transpose_modelview";
 
-const std::string DIFFUSE_MAP_MATRIX_PROPERTY = "s_diffuse_map_matrix";
-const std::string LIGHT_MAP_MATRIX_PROPERTY = "s_light_map_matrix";
-const std::string NORMAL_MAP_MATRIX_PROPERTY = "s_normal_map_matrix";
-const std::string SPECULAR_MAP_MATRIX_PROPERTY = "s_specular_map_matrix";
+const char* const DIFFUSE_MAP_MATRIX_PROPERTY = "s_diffuse_map_matrix";
+const char* const LIGHT_MAP_MATRIX_PROPERTY = "s_light_map_matrix";
+const char* const NORMAL_MAP_MATRIX_PROPERTY = "s_normal_map_matrix";
+const char* const SPECULAR_MAP_MATRIX_PROPERTY = "s_specular_map_matrix";
 
 }
