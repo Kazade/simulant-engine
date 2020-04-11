787138ae29e7b2ff616acaf021857ba62984b376
Luke Benstead <kazade@gmail.com>
2020-04-11 13:45:00 +0100

    Tweak android sources

--- simulant/platform.h
+++ simulant/platform.h
@@ -2,12 +2,14 @@
 #define PLATFORM_H
 
 #include <cstdint>
+#include <string>
 
 namespace smlt {
 
 class Platform {
 public:
     // Information
+    virtual ~Platform() {}
     virtual std::string name() const = 0;
 };
 
