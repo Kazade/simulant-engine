33df5f25d0adf8eed6751ae3c677c36cb503415d
freakdave <freakdave@hotmail.com>
2020-08-01 23:47:33 +0200

    Fixed: Also update utils header file

--- simulant/math/utils.h
+++ simulant/math/utils.h
@@ -11,16 +11,15 @@ const float PI_OVER_180 = PI / 180.0f;
 const float PI_UNDER_180 = 180.0f / PI;
 const float EPSILON = std::numeric_limits<float>::epsilon();
 
-float clamp(float x, float l, float h);
-float lerp(float x, float y, float t);
-float smoothstep(float e0, float e1, float x);
-float smootherstep(float e0, float e1, float x);
+float clamp(const float x, const float l, const float h);
+float lerp(const float x, const float y, const float t);
+float smoothstep(const float e0, const float e1, float x);
+float smootherstep(const float e0, const float e1, float x);
 
 template<typename T>
 bool almost_equal(const T& lhs, const T& rhs) {
     return lhs + std::numeric_limits<T>::epsilon() > rhs &&
            lhs - std::numeric_limits<T>::epsilon() < rhs;
 }
-
 
 }
