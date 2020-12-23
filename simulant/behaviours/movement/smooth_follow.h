32ff171e3f158e990798fa75c1bb19766adc6357
Luke Benstead <kazade@gmail.com>
2020-12-23 21:26:25 +0000

    Make sure we reset the target when it's destroyed

--- simulant/behaviours/movement/smooth_follow.h
+++ simulant/behaviours/movement/smooth_follow.h
@@ -16,6 +16,7 @@ class SmoothFollow:
 
 public:
     SmoothFollow();
+    ~SmoothFollow();
 
     const std::string name() const override {
         return "Smooth Follow";
@@ -24,12 +25,13 @@ public:
     void late_update(float dt) override;
     void set_target(ActorPtr actor);
     void set_target(ParticleSystemPtr ps);
+    bool has_target() const;
+    StageNode* target() const;
 
     void set_follow_distance(float dist) { distance_ = dist; }
     void set_follow_height(float height) { height_ = height; }
     void set_damping(float damping);
     void set_rotation_damping(float damping);
-
 private:
     StageNode* target_ = nullptr;
 
@@ -38,6 +40,8 @@ private:
 
     float damping_ = 5.0;
     float rotation_damping_ = 10.0;
+
+    sig::Connection destroy_conn_;
 
 };
 
