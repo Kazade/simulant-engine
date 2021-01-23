b0e0adf4826b3f9152ba16cd55db5ba715dea74a
David Reichelt <freakdave@hotmail.com>
2021-01-23 16:10:13 +0000

    Try another test scale for the octree culler

--- tests/test_octree_culler.h
+++ tests/test_octree_culler.h
@@ -24,7 +24,7 @@ public:
         mesh->new_submesh_as_box("not visible", mat2, 1.0, 1.0, 1.0, Vec3(20, 20, 20.0));
 
         OctreeCuller culler(nullptr, mesh, 4);
-        culler.compile(Vec3(), Quaternion(), Vec3(1.0, 1.0, 1.0));
+        culler.compile(Vec3(), Quaternion(), Vec3());
 
         auto bounds = culler.octree_bounds();
 
