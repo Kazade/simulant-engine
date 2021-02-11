5f70628b6b04a3a96089bc7955fd23a257fbe764
Luke Benstead <kazade@gmail.com>
2021-02-11 20:52:04 +0000

    Switch from FastVariant to dynamic-type switching to save RAM

--- simulant/assets/materials/material_property.h
+++ simulant/assets/materials/material_property.h
@@ -16,9 +16,61 @@
 
 namespace smlt {
 
+typedef FastVariant<bool, int, float, Vec2, Vec3, Vec4, Mat3, Mat4, TextureUnit> MaterialVariant;
+
 const MaterialPropertyID MATERIAL_PROPERTY_ID_INVALID = -1;
 
-typedef FastVariant<bool, int, float, Vec2, Vec3, Vec4, Mat3, Mat4, TextureUnit> MaterialVariant;
+namespace _material_impl {
+
+template<typename T>
+struct TypeToInt;
+
+template<>
+struct TypeToInt<bool> {
+    const static int value = 0;
+};
+
+template<>
+struct TypeToInt<int> {
+    const static int value = 1;
+};
+
+template<>
+struct TypeToInt<float> {
+    const static int value = 2;
+};
+
+template<>
+struct TypeToInt<Vec2> {
+    const static int value = 3;
+};
+
+template<>
+struct TypeToInt<Vec3> {
+    const static int value = 4;
+};
+
+template<>
+struct TypeToInt<Vec4> {
+    const static int value = 5;
+};
+
+template<>
+struct TypeToInt<Mat3> {
+    const static int value = 6;
+};
+
+template<>
+struct TypeToInt<Mat4> {
+    const static int value = 7;
+};
+
+template<>
+struct TypeToInt<TextureUnit> {
+    const static int value = 8;
+};
+
+}
 
 class MaterialObject;
 struct MaterialProperty;
@@ -33,21 +85,80 @@ public:
 
     MaterialPropertyValue() = default;
 
+    MaterialPropertyValue(const MaterialPropertyValue& rhs) {
+        (*this) = rhs;
+    }
+
+    MaterialPropertyValue& operator=(const MaterialPropertyValue& rhs) {
+        delete_data();
+
+        switch(rhs.stored_type_) {
+            case -1: {
+                stored_type_ = -1;
+                data_ = nullptr;
+            } break;
+            case _material_impl::TypeToInt<bool>::value: {
+                set_value(rhs.value<bool>());
+            } break;
+            case _material_impl::TypeToInt<int>::value: {
+                set_value(rhs.value<int>());
+            } break;
+            case _material_impl::TypeToInt<float>::value: {
+                set_value(rhs.value<float>());
+            } break;
+            case _material_impl::TypeToInt<Vec2>::value: {
+                set_value(rhs.value<Vec2>());
+            } break;
+            case _material_impl::TypeToInt<Vec3>::value: {
+                set_value(rhs.value<Vec3>());
+            } break;
+            case _material_impl::TypeToInt<Vec4>::value: {
+                set_value(rhs.value<Vec4>());
+            } break;
+            case _material_impl::TypeToInt<Mat3>::value: {
+                set_value(rhs.value<Mat3>());
+            } break;
+            case _material_impl::TypeToInt<Mat4>::value: {
+                set_value(rhs.value<Mat4>());
+            } break;
+            case _material_impl::TypeToInt<TextureUnit>::value: {
+                set_value(rhs.value<TextureUnit>());
+            } break;
+            default:
+                FATAL_ERROR(
+                    ERROR_CODE_INVALID_TYPE_ERROR,
+                    "Invalid type used in material property"
+                );
+        }
+
+        return *this;
+    }
+
     template<typename T>
     MaterialPropertyValue(MaterialProperty* property, const T& value):
-        property_(property),
-        variant_(value) {
+        property_(property) {
 
+        set_value(value);
+    }
+
+    ~MaterialPropertyValue() {
+        delete_data();
     }
 
     template<typename T>
     const T& value() const {
-        return variant_.get<T>();
+        assert(_material_impl::TypeToInt<T>::value == stored_type_);
+        assert(data_);
+
+        return *((T*) data_);
     }
 
     template<typename T>
     T& value() {
-        return variant_.get<T>();
+        assert(_material_impl::TypeToInt<T>::value == stored_type_);
+        assert(data_);
+
+        return *((T*) data_);
     }
 
     std::string shader_variable() const;
@@ -58,11 +169,75 @@ public:
 private:
     template<typename T>
     void set_value(const T& v) {
-        variant_.set(v);
+        if(stored_type_ > -1) {
+            delete_data();
+        }
+
+        assert(!data_);
+
+        stored_type_ = _material_impl::TypeToInt<T>::value;
+        T* new_thing = new T(v);
+        data_ = new_thing;
+    }
+
+    void delete_data() {
+        if(stored_type_== -1) {
+            assert(!data_);
+            return;
+        }
+
+        switch(stored_type_) {
+            case _material_impl::TypeToInt<bool>::value: {
+                bool* d = static_cast<bool*>(data_);
+                delete d;
+            } break;
+            case _material_impl::TypeToInt<int>::value: {
+                int* i = static_cast<int*>(data_);
+                delete i;
+            } break;
+            case _material_impl::TypeToInt<float>::value: {
+                float* f = static_cast<float*>(data_);
+                delete f;
+            } break;
+            case _material_impl::TypeToInt<Vec2>::value: {
+                Vec2* v = static_cast<Vec2*>(data_);
+                delete v;
+            } break;
+            case _material_impl::TypeToInt<Vec3>::value: {
+                Vec3* v = static_cast<Vec3*>(data_);
+                delete v;
+            } break;
+            case _material_impl::TypeToInt<Vec4>::value: {
+                Vec4* v = static_cast<Vec4*>(data_);
+                delete v;
+            } break;
+            case _material_impl::TypeToInt<Mat3>::value: {
+                Mat3* v = static_cast<Mat3*>(data_);
+                delete v;
+            } break;
+            case _material_impl::TypeToInt<Mat4>::value: {
+                Mat4* v = static_cast<Mat4*>(data_);
+                delete v;
+            } break;
+            case _material_impl::TypeToInt<TextureUnit>::value: {
+                TextureUnit* v = static_cast<TextureUnit*>(data_);
+                delete v;
+            } break;
+            default:
+                FATAL_ERROR(
+                    ERROR_CODE_INVALID_TYPE_ERROR,
+                    "Invalid type used in material property"
+                );
+        }
+
+        data_ = nullptr;
+        stored_type_ = -1;
     }
 
     MaterialProperty* property_ = nullptr;
-    MaterialVariant variant_;
+
+    int stored_type_ = -1;
+    void* data_ = nullptr;
 };
 
 struct MaterialPropertyValueEntry {
