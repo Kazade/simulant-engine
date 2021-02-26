2c7fde6c20a9409ab9c4d6b050cdefaa9680b0d4
Luke Benstead <kazade@gmail.com>
2021-02-26 09:02:09 +0000

    More to_string improvements

--- simulant/compat.cpp
+++ simulant/compat.cpp
@@ -3,44 +3,58 @@
 #include <cstdlib>
 
 #include "compat.h"
+#include "types.h"
 
 namespace smlt {
 
+const static int32_t _BUFFER_SIZE = 64;
+static char buffer[_BUFFER_SIZE] = {0};
+
 std::string to_string(int value) {
-    char buffer[64];
     auto c = ::sprintf(buffer, "%d", value);
     return std::string(buffer, buffer + c);
 }
 
 std::string to_string(unsigned value) {
-    char buffer[64];
     auto c = ::sprintf(buffer, "%u", value);
     return std::string(buffer, buffer + c);
 }
 
 std::string to_string(unsigned long value) {
-    char buffer[64];
     auto c = ::sprintf(buffer, "%luld", value);
     return std::string(buffer, buffer + c);
 }
 
 std::string to_string(long value) {
-    char buffer[64];
     auto c = ::sprintf(buffer, "%ld", value);
     return std::string(buffer, buffer + c);
 }
 
 std::string to_string(float value) {
-    char buffer[64];
     auto c = ::sprintf(buffer, "%f", (double) value);
     return std::string(buffer, buffer + c);
 }
 
 std::string to_string(double value) {
-    char buffer[64];
     auto c = ::sprintf(buffer, "%f", value);
     return std::string(buffer, buffer + c);
 }
+
+std::string to_string(const Vec2& v) {
+    auto c = ::sprintf(buffer, "(%f, %f)", v.x, v.y);
+    return std::string(buffer, buffer + c);
+}
+
+std::string to_string(const Vec3& v) {
+    auto c = ::sprintf(buffer, "(%f, %f, %f)", v.x, v.y, v.z);
+    return std::string(buffer, buffer + c);
+}
+
+std::string to_string(const Vec4& v) {
+    auto c = ::sprintf(buffer, "(%f, %f, %f, %f)", v.x, v.y, v.z, v.w);
+    return std::string(buffer, buffer + c);
+}
+
 
 int stoi(const std::string& str) {
     return atoi(str.c_str());
