9cfd6d6034175d49082c8cc03c26a0ee569d5257
Luke Benstead <kazade@gmail.com>
2021-03-03 21:08:38 +0000

    More stubbing

--- simulant/materials/material_property_overrider.h
+++ simulant/materials/material_property_overrider.h
@@ -2,6 +2,13 @@
 
 namespace smlt {
 
+/* All materials and passes inherit the properties and
+ * values of the baseline material. Overriders allow two things:
+ *
+ * 1. Overriding the value of the baseline material
+ * 2. Adding additional property values (e.g. shader uniforms)
+ */
+
 class MaterialPropertyOverrider {
 public:
     template<typename T>
@@ -13,7 +20,9 @@ public:
     template<typename T>
     void fetch_property_value(const char* name, T* out);
 
-
+    bool is_core_property(const char* name) const {
+        return BASELIN
+    }
 
 private:
     std::unordered_map<unsigned, Colour> colour_properties_;
