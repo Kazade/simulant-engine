2fa95e346a3ccba6624d9e794e8bdb1048b181be
Luke Benstead <kazade@gmail.com>
2021-10-19 14:26:15 +0100

    Add some assertions

--- simulant/assets/meshes/skeleton.h
+++ simulant/assets/meshes/skeleton.h
@@ -177,10 +177,16 @@ public:
     ) override;
 
     void set_joint_state_at_frame(std::size_t frame, std::size_t joint, JointState state) {
+        assert(frame < skeleton_frames_.size());
+        assert(joint < skeleton_frames_[frame].joints.size());
+
         skeleton_frames_[frame].joints[joint] = state;
     }
 
     const JointState& joint_state_at_frame(std::size_t frame, std::size_t joint) const {
+        assert(frame < skeleton_frames_.size());
+        assert(joint < skeleton_frames_[frame].joints.size());
+
         return skeleton_frames_[frame].joints[joint];
     }
 
