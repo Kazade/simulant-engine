78ce5d2a717d1b0aed98aa0c2580e4272d66dd2c
Luke Benstead <kazade@gmail.com>
2022-03-17 20:02:03 +0000

    Fix signed comparison

--- tests/test_vfs.h
+++ tests/test_vfs.h
@@ -10,13 +10,13 @@ public:
     void test_vfs_caching() {
         auto vfs = application->vfs.get();
         vfs->clear_location_cache();
-        assert_equal(vfs->location_cache_size(), 0);
+        assert_equal(vfs->location_cache_size(), 0u);
 
         auto f0 = vfs->locate_file("simulant/textures/simulant-icon.png");
-        assert_equal(vfs->location_cache_size(), 1);
+        assert_equal(vfs->location_cache_size(), 1u);
 
         auto f1 = vfs->locate_file("simulant/textures/simulant-icon.png");
-        assert_equal(vfs->location_cache_size(), 1);
+        assert_equal(vfs->location_cache_size(), 1u);
 
         assert_true(f0);
         assert_true(f1);
@@ -24,10 +24,10 @@ public:
 
         // Should purge the cache
         vfs->add_search_path("/bananas/");
-        assert_equal(vfs->location_cache_size(), 0);
+        assert_equal(vfs->location_cache_size(), 0u);
 
         vfs->locate_file("simulant/textures/simulant-icon.png");
-        assert_equal(vfs->location_cache_size(), 1);
+        assert_equal(vfs->location_cache_size(), 1u);
     }
 };
 
