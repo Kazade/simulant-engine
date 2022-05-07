5528b313471b3f5ea6445bb85ccc69adc84b45a9
Luke Benstead <kazade@gmail.com>
2022-05-07 18:12:02 +0100

    Add missing GL constants for PSP

--- simulant/renderers/gl_renderer.cpp
+++ simulant/renderers/gl_renderer.cpp
@@ -15,6 +15,21 @@
     #include "../../../deps/libgl/include/GL/glkos.h"
 #elif defined(__PSP__)
     #include <GL/gl.h>
+
+    /* PSP doesn't include these constants, we don't use them but need them for
+     * compilation */
+
+#define GL_PALETTE4_RGB8_OES              0x8B90
+#define GL_PALETTE4_RGBA8_OES             0x8B91
+#define GL_PALETTE4_R5_G6_B5_OES          0x8B92
+#define GL_PALETTE4_RGBA4_OES             0x8B93
+#define GL_PALETTE4_RGB5_A1_OES           0x8B94
+#define GL_PALETTE8_RGB8_OES              0x8B95
+#define GL_PALETTE8_RGBA8_OES             0x8B96
+#define GL_PALETTE8_R5_G6_B5_OES          0x8B97
+#define GL_PALETTE8_RGBA4_OES             0x8B98
+#define GL_PALETTE8_RGB5_A1_OES           0x8B99
+
 #else
     #include "./glad/glad/glad.h"
 #endif
