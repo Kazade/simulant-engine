a256ae102c9d2f3156c36ecc14e47cfe49cfd299
David Reichelt <freakdave@hotmail.com>
2023-02-12 16:18:52 +0000

    Final cleanups part 2

--- simulant/assets/particles/direction_noise_random_manipulator.h
+++ simulant/assets/particles/direction_noise_random_manipulator.h
@@ -6,7 +6,7 @@ namespace smlt {
 
 class DirectionNoiseRandomManipulator : public Manipulator {
 public:
-    DirectionNoiseRandomManipulator(ParticleScript* script, const smlt::Vec3& dir, const smlt::Vec3& noise_amount):
+    DirectionNoiseRandomManipulator(ParticleScript* script, const Vec3& dir, const Vec3& noise_amount):
         Manipulator(script, "direction_noise_random"),
         dir_(dir),
         noise_amount_(noise_amount) {}
@@ -17,8 +17,8 @@ private:
         Particle* particles,
         std::size_t particle_count, float dt) const override;
 
-    smlt::Vec3 dir_;
-    smlt::Vec3 noise_amount_;
+    Vec3 dir_;
+    Vec3 noise_amount_;
 };
 
 
