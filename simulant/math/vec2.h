3cd52c392023ed0159fe3fb759a0a34300b1df41
Dave Reichelt <freakdave@hotmail.com>
2023-02-17 22:34:56 +0100

    Revert "Remove FIPR from vec2 and replace with standard dot calculation"
    This reverts commit b8b3eb17959eec1e9e4ee61405633211e38d0ab4.

--- simulant/math/vec2.h
+++ simulant/math/vec2.h
@@ -46,7 +46,7 @@ public:
 
     float length_squared() const {
 #ifdef __DREAMCAST__
-        return x * x + y * y;
+        return MATH_fipr(x, y, 0.0f, 0.0f, x, y, 0.0f, 0.0f);
 #else
         return dot(*this);
 #endif
@@ -116,7 +116,11 @@ public:
     }
 
     float dot(const Vec2& rhs) const {       
+#ifdef __DREAMCAST__
+        return MATH_fipr(x, y, 0.0f, 0.0f, rhs.x, rhs.y, 0.0f, 0.0f);
+#else
         return x * rhs.x + y * rhs.y;
+#endif
     }
 
     Vec3 xyz(float z = 0.0f) const;
