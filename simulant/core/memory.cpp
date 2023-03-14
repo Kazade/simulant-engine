39e822a175911fffdf899dc2c3e63834eace2c22
Luke Benstead <kazade@gmail.com>
2023-03-14 21:05:53 +0000

    Fix Dreamcast build

--- simulant/core/memory.cpp
+++ simulant/core/memory.cpp
@@ -1,3 +1,5 @@
+#include <malloc.h>
+
 #include "memory.h"
 
 void* smlt::aligned_alloc(std::size_t alignment, std::size_t size) {
@@ -5,6 +7,8 @@ void* smlt::aligned_alloc(std::size_t alignment, std::size_t size) {
 
 #ifdef __WIN32__
     p = _aligned_malloc(size, alignment);
+#elif defined(__DREAMCAST__)
+    p = memalign(alignment, size);
 #else
     if(posix_memalign(&p, alignment, size) != 0) {
         p = nullptr;
