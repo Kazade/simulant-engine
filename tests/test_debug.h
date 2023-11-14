03e2df28bc3a417efbc3b94ad4cb868efd266b94
Luke Benstead <kazade@gmail.com>
2023-11-14 20:34:29 +0000

    Add missing test file

---
+++ tests/test_debug.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "simulant/simulant.h"
+
+
+namespace {
+
+class DebugTests : public smlt::test::SimulantTestCase {
+public:
+    void test_debug_can_be_created() {
+        auto debug = scene->create_child<Debug>();
+        assert_is_not_null(debug);
+        assert_equal(scene->count_nodes_by_type<Debug>(), 1u);
+        debug->destroy();
+        assert_equal(scene->count_nodes_by_type<Debug>(), 0u);
+    }
+};
+
+}
