2cfed798b0bd2cf29cf002ad5b6583abeb748188
Luke Benstead <kazade@gmail.com>
2024-01-20 10:00:56 +0000

    Add a test

--- tests/test_sphere_approximation.h
+++ tests/test_sphere_approximation.h
@@ -11,8 +11,15 @@ class SphereApproximationTests : public test::SimulantTestCase {
 public:
 
     void test_basic_usage() {
+        auto mesh = scene->assets->create_mesh(VertexSpecification::DEFAULT);
 
+        auto approx1 = mesh->generate_sphere_approximation();
+        assert_equal(approx1.sphere_count(), 0u);
 
+        mesh->create_submesh_as_sphere("Sphere", scene->assets->clone_default_material(), 1.0f, 5, 5);
+
+        auto approx2 = mesh->generate_sphere_approximation();
+        assert_equal(approx2.sphere_count(), 1u);
     }
 };
 
