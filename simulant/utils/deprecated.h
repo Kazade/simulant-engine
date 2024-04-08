b3b02b5efc384357312374c27e767539b939f386
Luke Benstead <kazade@gmail.com>
2024-04-08 21:07:51 +0100

    Fix deprecated macro

--- simulant/utils/deprecated.h
+++ simulant/utils/deprecated.h
@@ -1,14 +1,14 @@
 #pragma once
 
 #if __cplusplus==201402L
-#define DEPRECATED [[deprecated]]
+#define S_DEPRECATED [[deprecated]]
 #else
     #ifdef __GNUC__
-    #define DEPRECATED __attribute__((deprecated))
-    #elif defined(_MSC_VER)
-    #define DEPRECATED __declspec(deprecated)
-    #else
-    #pragma message("WARNING: You need to implement DEPRECATED for this compiler")
-    #define DEPRECATED
-    #endif
+#define S_DEPRECATED __attribute__((deprecated))
+#elif defined(_MSC_VER)
+#define S_DEPRECATED __declspec(deprecated)
+#else
+#pragma message("WARNING: You need to implement DEPRECATED for this compiler")
+#define S_DEPRECATED
+#endif
 #endif
