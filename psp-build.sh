8f8ec98a9e0261744f885109e34c842ec8841654
Luke Benstead <kazade@gmail.com>
2024-05-17 19:35:25 +0100

    Tweak compile flags

--- psp-build.sh
+++ psp-build.sh
@@ -1,1 +1,1 @@
-docker run -v `pwd`:`pwd`:Z kazade/psp-sdk /bin/sh -c "cd `pwd`/pbuild; psp-cmake -DCMAKE_BUILD_TYPE=Release -DSIMULANT_PROFILE=ON .. && make -j12"
+docker run -v `pwd`:`pwd`:Z kazade/psp-sdk /bin/sh -c "cd `pwd`/pbuild; psp-cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_PRX=1 -DENC_PRX=1 -DSIMULANT_PROFILE=ON .. && make -j12"
