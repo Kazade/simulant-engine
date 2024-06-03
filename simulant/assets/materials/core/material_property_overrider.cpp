8164a69e8e25a5a955e8d1217648f57c7afc7d0d
Dave Reichelt <freakdave@hotmail.com>
2024-06-03 17:39:25 +0200

    Make sure we use the new _S_RESTRICT macro

--- simulant/assets/materials/core/material_property_overrider.cpp
+++ simulant/assets/materials/core/material_property_overrider.cpp
@@ -1,5 +1,6 @@
 #include "material_property_overrider.h"
 #include "../../../logging.h"
+#include "../../../macros.h"
 
 namespace smlt {
 
@@ -20,11 +21,7 @@ bool valid_name(const char* name) {
 }
 
 template<typename T, typename Map>
-#if !defined(_MSC_VER)
-static inline bool fetcher(const MaterialPropertyOverrider* const __restrict__ _this, const MaterialPropertyOverrider* const __restrict__ parent, const Map& map, MaterialPropertyNameHash hsh, const T*& out) {
-#else
-static inline bool fetcher(const MaterialPropertyOverrider* const __restrict _this, const MaterialPropertyOverrider* const __restrict parent, const Map & map, MaterialPropertyNameHash hsh, const T * &out) {
-#endif
+static inline bool fetcher(const MaterialPropertyOverrider* const _S_RESTRICT _this, const MaterialPropertyOverrider* const __restrict parent, const Map & map, MaterialPropertyNameHash hsh, const T * &out) {
     auto& lookup = _this->*map;
 
     auto s = lookup.size();
