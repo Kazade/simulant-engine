c3240019a3a23e3035900e69ca46aa3b72c6307b
Luke Benstead <kazade@gmail.com>
2024-06-14 20:33:14 +0100

    Add Camera::set_projection_matrix

--- simulant/nodes/camera.cpp
+++ simulant/nodes/camera.cpp
@@ -1,25 +1,23 @@
 #include "camera.h"
 #include "actor.h"
 
-#include "../stage.h"
-#include "../window.h"
-#include "../viewport.h"
 #include "../application.h"
+#include "../stage.h"
+#include "../viewport.h"
+#include "../window.h"
 
 namespace smlt {
 
-
-Camera::Camera(Scene *owner):
+Camera::Camera(Scene* owner) :
     ContainerNode(owner, STAGE_NODE_TYPE_CAMERA) {
 
     assert(owner);
 
-    set_perspective_projection(smlt::Degrees(45.0f), get_app()->window->aspect_ratio());
+    set_perspective_projection(smlt::Degrees(45.0f),
+                               get_app()->window->aspect_ratio());
 }
 
-Camera::~Camera() {
-
-}
+Camera::~Camera() {}
 
 void Camera::on_transformation_changed() {
     StageNode::on_transformation_changed();
@@ -27,35 +25,45 @@ void Camera::on_transformation_changed() {
 }
 
 void Camera::update_frustum() {
-    //Recalculate the view matrix
-    view_matrix_ = Mat4::as_look_at(
-        transform->position(),
-        transform->position() + transform->orientation().forward(),
-        transform->orientation().up()
-    );
+    // Recalculate the view matrix
+    view_matrix_ = Mat4::as_look_at(transform->position(),
+                                    transform->position() +
+                                        transform->orientation().forward(),
+                                    transform->orientation().up());
 
     Mat4 mvp = projection_matrix_ * view_matrix_;
 
-    frustum_.build(&mvp); //Update the frustum for this camera
+    frustum_.build(&mvp); // Update the frustum for this camera
 }
 
-void Camera::set_perspective_projection(const Degrees& fov, float aspect, float near, float far) {
-    projection_matrix_ = Mat4::as_projection(fov, aspect, near, far);
+void Camera::set_projection_matrix(const Mat4& matrix) {
+    projection_matrix_ = matrix;
     update_frustum();
 }
 
-void Camera::set_orthographic_projection(float left, float right, float bottom, float top, float near, float far) {
-    projection_matrix_ = Mat4::as_orthographic(left, right, bottom, top, near, far);
-    update_frustum();
+void Camera::set_perspective_projection(const Degrees& fov, float aspect,
+                                        float near, float far) {
+    set_projection_matrix(Mat4::as_projection(fov, aspect, near, far));
 }
 
-float Camera::set_orthographic_projection_from_height(float desired_height_in_units, float ratio) {
+void Camera::set_orthographic_projection(float left, float right, float bottom,
+                                         float top, float near, float far) {
+    set_projection_matrix(
+        Mat4::as_orthographic(left, right, bottom, top, near, far));
+}
+
+float Camera::set_orthographic_projection_from_height(
+    float desired_height_in_units, float ratio) {
     float width = desired_height_in_units * ratio;
-    set_orthographic_projection(-width / 2.0f, width / 2.0f, -desired_height_in_units / 2.0f, desired_height_in_units / 2.0f, -10.0f, 10.0f);
+    set_orthographic_projection(-width / 2.0f, width / 2.0f,
+                                -desired_height_in_units / 2.0f,
+                                desired_height_in_units / 2.0f, -10.0f, 10.0f);
     return width;
 }
 
-smlt::optional<Vec3> Camera::project_point(const RenderTarget &target, const Viewport &viewport, const Vec3& point) const {
+smlt::optional<Vec3> Camera::project_point(const RenderTarget& target,
+                                           const Viewport& viewport,
+                                           const Vec3& point) const {
     Vec4 in(point, 1.0);
     Vec4 out = view_matrix() * in;
     in = projection_matrix() * out;
@@ -77,14 +85,13 @@ smlt::optional<Vec3> Camera::project_point(const RenderTarget &target, const Vie
     float vp_xoffset = target.width() * viewport.x();
     float vp_yoffset = target.height() * viewport.y();
 
-    return Vec3(
-        in.x * vp_width + vp_xoffset,
-        in.y * vp_height + vp_yoffset,
-        in.z
-    );
+    return Vec3(in.x * vp_width + vp_xoffset, in.y * vp_height + vp_yoffset,
+                in.z);
 }
 
-smlt::optional<Vec3> Camera::unproject_point(const RenderTarget& target, const Viewport& viewport, const Vec3& win) {
+smlt::optional<Vec3> Camera::unproject_point(const RenderTarget& target,
+                                             const Viewport& viewport,
+                                             const Vec3& win) {
     /*
      * WARNING: This function is untested (FIXME) !!!
      */
@@ -97,8 +104,8 @@ smlt::optional<Vec3> Camera::unproject_point(const RenderTarget& target, const V
 
     float vx = float(target.width()) * viewport.x();
     float vy = float(target.height()) * viewport.y();
-    float vw = (float) viewport.width_in_pixels(target);
-    float vh = (float) viewport.height_in_pixels(target);
+    float vw = (float)viewport.width_in_pixels(target);
+    float vh = (float)viewport.height_in_pixels(target);
 
     in.x = (win.x - vx) / vw * 2.0f - 1.0f;
     in.y = (win.y - vy) / vh * 2.0f - 1.0f;
@@ -122,4 +129,4 @@ smlt::optional<Vec3> Camera::unproject_point(const RenderTarget& target, const V
     return smlt::optional<Vec3>(std::move(ret));
 }
 
-}
+} // namespace smlt
--- simulant/nodes/camera.h
+++ simulant/nodes/camera.h
@@ -1,21 +1,19 @@
 #pragma once
 
-#include "stage_node.h"
-#include "../generic/identifiable.h"
-#include "../generic/optional.h"
-#include "../generic/manual_object.h"
-#include "../math/aabb.h"
 #include "../frustum.h"
+#include "../generic/identifiable.h"
+#include "../generic/manual_object.h"
+#include "../generic/optional.h"
+#include "../math/aabb.h"
 #include "../sound.h"
+#include "stage_node.h"
 
 namespace smlt {
 
 class RenderTarget;
 class CameraParams {};
 
-class Camera:
-    public ContainerNode,
-    public ChainNameable<Camera> {
+class Camera: public ContainerNode, public ChainNameable<Camera> {
 
 public:
     struct Meta {
@@ -28,7 +26,8 @@ public:
     Camera(Scene* owner);
     virtual ~Camera();
 
-    /* Camera Proxies have no mass/body so their AABB is just 0,0,0, or their position */
+    /* Camera Proxies have no mass/body so their AABB is just 0,0,0, or their
+     * position */
     const AABB& aabb() const override {
         return bounds_;
     }
@@ -38,20 +37,38 @@ public:
     }
 
     // Converts an OpenGL unit to window space
-    smlt::optional<Vec3> project_point(const RenderTarget& target, const Viewport& viewport, const Vec3& point) const;
+    smlt::optional<Vec3> project_point(const RenderTarget& target,
+                                       const Viewport& viewport,
+                                       const Vec3& point) const;
 
-    // Converts a pixel to OpenGL units (z-input should be read from the depth buffer)
-    smlt::optional<Vec3> unproject_point(const RenderTarget& target, const Viewport& viewport, const Vec3& win_point);
+    // Converts a pixel to OpenGL units (z-input should be read from the depth
+    // buffer)
+    smlt::optional<Vec3> unproject_point(const RenderTarget& target,
+                                         const Viewport& viewport,
+                                         const Vec3& win_point);
 
-    const Mat4& view_matrix() const { return view_matrix_; }
-    const Mat4& projection_matrix() const { return projection_matrix_; }
+    const Mat4& view_matrix() const {
+        return view_matrix_;
+    }
+    const Mat4& projection_matrix() const {
+        return projection_matrix_;
+    }
 
-    Frustum& frustum() { return frustum_; }
-    const Frustum& frustum() const { return frustum_; }
+    Frustum& frustum() {
+        return frustum_;
+    }
+    const Frustum& frustum() const {
+        return frustum_;
+    }
 
-    void set_perspective_projection(const Degrees &fov, float aspect, float near=1.0f, float far=1000.0f);
-    void set_orthographic_projection(float left, float right, float bottom, float top, float near=-1.0, float far=1.0);
-    float set_orthographic_projection_from_height(float desired_height_in_units, float ratio);
+    void set_projection_matrix(const Mat4& matrix);
+    void set_perspective_projection(const Degrees& fov, float aspect,
+                                    float near = 1.0f, float far = 1000.0f);
+    void set_orthographic_projection(float left, float right, float bottom,
+                                     float top, float near = -1.0,
+                                     float far = 1.0);
+    float set_orthographic_projection_from_height(float desired_height_in_units,
+                                                  float ratio);
 
     bool on_create(void* params) override {
         _S_UNUSED(params);
@@ -70,8 +87,7 @@ private:
     void on_transformation_changed() override;
 };
 
+class Camera2D: public Camera {};
+class Camera3D: public Camera {};
 
-class Camera2D : public Camera {};
-class Camera3D : public Camera {};
-
-}
+} // namespace smlt
