7056e4d8d4ea8d521fea84bd3765f286fc073ef3
Luke Benstead <kazade@gmail.com>
2024-11-30 14:42:50 +0000

    Clean up and fix the boxes sample

--- samples/boxes.cpp
+++ samples/boxes.cpp
@@ -19,6 +19,7 @@ public:
         );
 
         pipeline_->viewport->set_color(smlt::Color::blue());
+        pipeline_->set_clear_flags(smlt::BUFFER_CLEAR_ALL);
 
         camera_->set_perspective_projection(
             Degrees(45.0), float(window->width()) / float(window->height()), 1.0, 1000.0
@@ -28,17 +29,19 @@ public:
 
         // Create a nice skybox (not on DC, the image is too big)
         if(get_platform()->name() != "dreamcast") {
-            create_child<Skybox>("sample_data/skyboxes/TropicalSunnyDay");
+            create_child<Skybox>("assets/samples/skyboxes/TropicalSunnyDay");
         }
 
-        auto crate = app->shared_assets->load_texture("sample_data/crate.png");
+        auto crate =
+            app->shared_assets->load_texture("assets/samples/crate.png");
         auto mat = app->shared_assets->create_material_from_texture(crate);
 
         auto box_mesh = app->shared_assets->create_mesh(smlt::VertexSpecification::DEFAULT, smlt::GARBAGE_COLLECT_NEVER);
         box_mesh->create_submesh_as_cube("cube", mat, 5);
         box_mesh_ = box_mesh;
 
-        auto grass = app->shared_assets->load_texture("sample_data/beach_sand.png");
+        auto grass =
+            app->shared_assets->load_texture("assets/samples/beach_sand.png");
         auto ground_mesh = app->shared_assets->create_mesh(smlt::VertexSpecification::DEFAULT);
         ground_mesh->create_submesh_as_box(
             "ground", app->shared_assets->create_material_from_texture(grass), 1000, 2.5, 1000
@@ -48,12 +51,9 @@ public:
         ground_ = create_child<smlt::Actor>(ground_mesh_);
 
         // Make the ground a staticbody
-        auto c = create_child<smlt::StaticBody>();
-        c->adopt_children(ground_); // FIXME: Convert to mixin
+        auto c = ground_->create_mixin<smlt::StaticBody>();
         c->add_box_collider(ground_->aabb().dimensions(),
                             PhysicsMaterial::stone());
-
-        srand(time(nullptr));
     }
 
     void spawn_box() {
@@ -61,10 +61,9 @@ public:
 
         auto box = boxes_.back();
         auto pos = Vec3(
-            ((float(rand()) / RAND_MAX) * 20.0f) - 10.0f,
-            20,
-            ((float(rand()) / RAND_MAX) * 10.0f) - 5.0f
-        );
+            smlt::RandomGenerator::instance().float_in_range(-20.0f, 20.0f),
+            smlt::RandomGenerator::instance().float_in_range(20.0f, 30.0f),
+            smlt::RandomGenerator::instance().float_in_range(-10.0f, 10.0f));
 
         auto controller = create_child<smlt::DynamicBody>(pos);
         controller->add_box_collider(box->aabb().dimensions(),
@@ -79,6 +78,11 @@ public:
         if(counter > 1.0f) {
             counter = 0;
             spawn_box();
+
+            if(boxes_.size() > 10) {
+                boxes_.front()->destroy();
+                boxes_.erase(boxes_.begin());
+            }
         }
     }
 
